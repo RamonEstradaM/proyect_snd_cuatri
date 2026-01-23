4acabfb3e75b20538c0c70c861f31fe3d8ce4e2a
RamonEstradaM <Ramon.Manriquez@cinvestav.mx>
2026-01-23 22:22:24 +0000

    changes in the testbench

--- rtl/control_pid.sv
+++ rtl/control_pid.sv
@@ -1,4 +1,11 @@
-module control_pid(
+module control_pid #(
+	//coeficients control PID 
+	parameter int KP = 100,
+	parameter int KI = 1,
+	parameter int KD = 10,
+	parameter int SAMPLE_DIV = 50000
+
+)(
 
 	input logic clk,
 	input logic rst_n,
@@ -7,26 +14,21 @@ module control_pid(
 	output logic [17:0] duty_out    //value for the pwm
 );
 
-	//coeficients control PID
-	parameter int KP = 100;
-	parameter int KI = 1;
-	parameter int KD = 10;
-
 	logic signed [31:0] error;
 	logic signed [31:0] last_error;
 	logic signed [31:0] control_val;
 	logic signed [31:0] integral;
 	logic signed [31:0] derivative;
 
-	localparam signed [31:0] MIN_DUTY = 50000; //in grades = 0
-	localparam signed [31:0] OFFSET = 75000; //in grades = 90
-	localparam signed [31:0] MAX_DUTY= 100000; // in grades = 180
+	localparam signed [31:0] MIN_DUTY = 32'd50000; //in grades = 0
+	localparam signed [31:0] OFFSET = 32'd75000; //in grades = 90
+	localparam signed [31:0] MAX_DUTY= 32'd100000; // in grades = 180
 
 	always_ff @(posedge clk or negedge rst_n) begin
 		if(!rst_n) begin
 			integral <= 0;
 			last_error <= 0;
-			duty_out <= 75000; //return to center
+			duty_out <= 18'd75000; //return to center
 		end else begin 
 			error = $signed({1'b0, gtob_out}) - $signed({1'b0, position_b_out});
 
