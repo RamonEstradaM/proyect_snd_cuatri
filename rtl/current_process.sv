f90e394bb8953b28bcd802ee5e60f40961ebf511
RamonEstradaM <Ramon.Manriquez@cinvestav.mx>
2026-01-29 22:17:21 +0000

    modified current process

--- rtl/current_process.sv
+++ rtl/current_process.sv
@@ -1,28 +1,32 @@
 module current_process#(
 	parameter [11:0] CURRENT_MAX = 12'd2500, //current in 2V
-	parameter int TIME_LIMIT = 1000 //cycles of wait before of off 20ms
+	parameter TIME_LIMIT = 1000 //cycles of wait before of off 20ms
 )(
 	input logic clk,
 	input logic rst_n,
 	input logic [11:0] current_b_out,
 	output logic current_high
 );
-	int time_current_high;
+	logic [($clog2(TIME_LIMIT + 1))-1 : 0] time_current_high;
 	
 	always_ff @(posedge clk or negedge rst_n) begin
 		if(!rst_n) begin
 			time_current_high <= 0;
-			current_high <= 1'b1;
+			current_high <= 1'b0;
 		end else begin
-			if (CURRENT_MAX > current_b_out)begin
-				time_current_high <= time_current_high + 1;
-				if(time_current_high > TIME_LIMIT)
-					current_high <= 1'b1;
-				else
+			if (current_b_out > CURRENT_MAX)begin  //if over current
+				if(TIME_LIMIT > time_current_high)begin
+					time_current_high <= time_current_high + 1'b1;
 					current_high <= 1'b0;
-			end else
+				end else if(time_current_high >= TIME_LIMIT) begin
+					current_high <= 1'b1;
+				end else 
+					current_high <= 1'b0;
+
+			end else begin
+				time_current_high <= '0;
 				current_high <= 1'b0;
-				time_current_high <= 1'b0;
+			end
 		end
 	end
 endmodule
