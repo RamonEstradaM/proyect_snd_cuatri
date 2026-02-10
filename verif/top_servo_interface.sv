54eb788262d30c60a16efe39ee452b215b5232a9
Ramon Estrada <Ramon.Manriquez@cinvestav.mx>
2026-02-10 16:52:56 -0600

    Add servo_interface

---
+++ verif/top_servo_interface.sv
@@ -0,0 +1,31 @@
+interface servo_interface(input logic clk);
+    logic        rst_n;
+    real         grades, measure_current, measure_grades;
+    logic        pwm_out;
+
+    task reset_dut();
+        rst_n <= 1'b1;
+        #10 rst_n <= 1'b0;
+        #20 rst_n <= 1'b1;
+    endtask
+
+    task move_to(input real target);
+        @(posedge clk);
+        grades <= target;
+    endtask
+
+    task force_fault(input real value);
+        @(posedge clk);
+        measure_current <= value;
+    endtask
+
+    task run_motor_sim(input real speed);
+        forever begin
+            @(posedge clk);
+            if (rst_n) begin
+                if (measure_grades < grades - 0.5)      measure_grades <= measure_grades + speed;
+                else if (measure_grades > grades + 0.5) measure_grades <= measure_grades - speed;
+            end
+        end
+    endtask
+endinterface
